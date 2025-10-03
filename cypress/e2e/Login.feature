e0cfbaa7f4c1a44e347d62bf7f4ef83e0cb47758
gloriayordanova <raevagloria@gmail.com>
2025-10-03 15:03:02 +0100

    Final Version

--- cypress/e2e/Login.feature
+++ cypress/e2e/Login.feature
@@ -1,11 +1,11 @@
 Feature: LoginHRM - Login Page
 
     Scenario Outline: Validate valid login page
-        Given I navigate to the OrangeHRM login page
-        And I type a username <username> and password <password>
-        # And I click on the login button
-        # Then I should see the dashboard page
+        Given I navigate to the SauceDemo login page
+        And I type a username "<username>" and password "<password>"
+        And I click on the login button
+        Then I should see the dashboard page
 
         Examples:
-            | username | password |
-            | Admin    | admin123 |
+            | username      | password     |
+            | standard_user | secret_sauce |
--- cypress/support/step_definitions/Login.js
+++ cypress/support/step_definitions/Login.js
@@ -1,13 +1,23 @@
 /// <reference types="cypress" />
 import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor";
 
-const url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+const url = "https://www.saucedemo.com/";
 
-    Given ('I navigate to the OrangeHRM login page', () => {
-        cy.visit(url);
-    })
+Given('I navigate to the SauceDemo login page', () => {
+    cy.visit(url);
+});
 
-    When ('I type a username {string} and password {string}', (username, password) => {
-        cy.get('input[name="username"]').type(username);
-        cy.get('input[name="password"]').type(password);
-    })
+When('I type a username {string} and password {string}', (username, password) => {
+    cy.get('input[data-test="username"]').type(username);
+    cy.get('[data-test="password"]').type(password);
+});
+
+When('I click on the login button', () => {
+    cy.get('[data-test="login-button"]').click();
+});
+
+Then('I should see the dashboard page', () => {
+    // Check that the URL is correct
+    cy.url().should('eq', `${url}inventory.html`);
+});
+
