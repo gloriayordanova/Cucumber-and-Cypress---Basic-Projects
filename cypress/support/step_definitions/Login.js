a70a3ad907796ce0a60ebde1b4dd0bb267644117
gloriayordanova <raevagloria@gmail.com>
2025-10-03 11:08:38 +0100

    Final Version

--- cypress/support/step_definitions/Login.js
+++ cypress/support/step_definitions/Login.js
@@ -1,3 +1,8 @@
 /// <reference types="cypress" />
 import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor";
 
+const url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+
+Given ('I navigate to the OrangeHRM login page', () => {
+    cy.visit(url);
+})
