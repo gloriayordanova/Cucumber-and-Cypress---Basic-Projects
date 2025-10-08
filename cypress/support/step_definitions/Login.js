607e0b26f0ce7c7d71dbfb56d5ac487050a4286c
gloriayordanova <raevagloria@gmail.com>
2025-10-08 16:35:37 +0100

    Final Version

--- cypress/support/step_definitions/Login.js
+++ cypress/support/step_definitions/Login.js
@@ -1,30 +1,39 @@
 /// <reference types="cypress" />
-import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor";
+import { Given, Then, When } from '@badeball/cypress-cucumber-preprocessor'
+import Base_PO from '../page_objects/Base_PO'
 
-const url = "https://www.saucedemo.com/";
+const basePage = new Base_PO()
+
+// const url = "https://www.saucedemo.com/";
 
 Given('I navigate to the SauceDemo login page', () => {
-    cy.visit(url);
-});
+  // cy.visit(url);
+  basePage.navigate('')
+})
 
-When('I type a username {string} and password {string}', (username, password) => {
-    cy.get('input[data-test="username"]').type(username);
-    cy.get('[data-test="password"]').type(password);
-});
+When(
+  'I type a username {string} and password {string}',
+  (username, password) => {
+    cy.get('input[data-test="username"]').type(username)
+    cy.get('[data-test="password"]').type(password)
+  }
+)
 
 When('I click on the login button', () => {
-    cy.get('[data-test="login-button"]').click();
-});
+  cy.get('[data-test="login-button"]').click()
+})
 
 Then('I should see the dashboard page', () => {
-    // Check that the URL is correct
-    cy.url().should('eq', `${url}inventory.html`);
-});
+  // Check that the URL is correct
+  cy.url().should('include', 'inventory.html')
+})
 
 When('I type a username {string}', (username) => {
-    cy.get('input[data-test="username"]').type(username);
-});
+  cy.get('input[data-test="username"]').type(username)
+})
 
 Then('I should see the error message for password required', () => {
-    cy.get('.error-message-container').contains('Epic sadface: Password is required');
-});
+  cy.get('.error-message-container').contains(
+    'Epic sadface: Password is required'
+  )
+})
